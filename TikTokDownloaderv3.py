5f0ff9909b991e6cd84301e4a8e77aa55fdfa3ff
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-28 12:50:35 +0200

    Fixed

--- TikTokDownloaderv3.py
+++ TikTokDownloaderv3.py
@@ -1,6 +1,5 @@
 import requests
 import re
-<<<<<<< HEAD
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import PhotoImage
@@ -115,54 +114,3 @@ def create_gui():
 
 if __name__ == '__main__':
     create_gui()
-=======
-
-USER_AGENT = {
-    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.57"
-}
-
-def get_tiktok_video_id(url):
-    mobile_pattern = re.compile(r'(https?://[^\s]+tiktok.com/[^\s@]+)')
-    web_pattern = re.compile(r'(https?://www.tiktok.com/@[^\s]+/video/[0-9]+)')
-    
-    if match := mobile_pattern.search(url) or web_pattern.search(url):
-        if match.group().startswith('https://www'):
-            return match.group().split('/')[-1]
-        else:
-            try:
-                response = requests.get(url, headers=USER_AGENT)
-                response.raise_for_status()  # Raises an HTTPError for bad responses
-                id_url = re.search(r'(https?://www.tiktok.com/@[^\s]+/video/[0-9]+)', response.text).group()
-                return id_url.split('/')[-1]
-            except requests.RequestException as e:
-                print(f"Error during requests to {url}: {e}")
-    else:
-        print('Incorrect TikTok URL format.')
-        return None
-
-def download_video(video_link, filename):
-    try:
-        response = requests.get(video_link, headers=USER_AGENT)
-        response.raise_for_status()  # Raises an HTTPError for bad responses
-        with open(f'{filename}.mp4', 'wb') as f:
-            f.write(response.content)
-    except requests.RequestException as e:
-        print(f"Error downloading video: {e}")
-
-def download_tiktok(url):
-    tiktok_id = get_tiktok_video_id(url)
-    if tiktok_id:
-        # Assuming the toolav.herokuapp.com service is up and returns a direct video URL in the expected format
-        dl_url = f'https://toolav.herokuapp.com/id/?video_id={tiktok_id}'
-        try:
-            response = requests.get(dl_url)
-            response.raise_for_status()  # Check if request was successful
-            video_link = response.json()['item']['video']['playAddr']
-            download_video(video_link, tiktok_id)
-        except requests.RequestException as e:
-            print(f"Error retrieving video download link: {e}")
-
-if __name__ == '__main__':
-    url = input('Enter TikTok URL: ')
-    download_tiktok(url)
->>>>>>> 085a52af76d0afb18ff0488847c608aee96f2c7e
